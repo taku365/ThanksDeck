8d2427ec99a44063816866071ff775d07e94f86e
taku365 <stcd1097@gmail.com>
2025-06-21 14:31:01 +0900

    Material-UI@v5とemotionの導入

---
+++ next/src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material'
+
+const theme = createTheme({
+  // typography: {
+  //   fontFamily: 'Noto Sans JP, Roboto, "Helvetica Neue", Arial, sans-serif',
+  // },
+  palette: {
+    primary: {
+      main: '#FDF7E8',
+    },
+    secondary: {
+      main: '#FBBF23',
+    },
+    error: {
+      main: '#E63946',
+    },
+  },
+})
+
+export default theme
