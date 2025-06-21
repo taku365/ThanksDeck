9922b56e2bde6d65d9dbfd051119b912b0108685
taku365 <stcd1097@gmail.com>
2025-06-21 15:05:49 +0900

    Rails API と Next.js の疎通

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -1,16 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins Settings.front_domain # 環境に応じた設定ファイルに切り替え,本番環境は実際のドメイン（実装はまだ）
+
+    resource "*",
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
