7f460111bd52ef6873e4e1b9588447c5fe430bfb
taku365 <stcd1097@gmail.com>
2025-06-22 19:33:12 +0900

    updateアクションを追加

--- rails/app/controllers/api/v1/cards_controller.rb
+++ rails/app/controllers/api/v1/cards_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::CardsController < Api::V1::BaseController
     render json: serialize(card), status: :ok
   end
 
-  # POST /api/v1/cards
+  # POST /api/v1/cards カードの作成
   def create
     # 今日の作成済みカード数をカウント
     today_count = current_user.cards.where(logged_date: Time.zone.today).count
@@ -34,6 +34,17 @@ class Api::V1::CardsController < Api::V1::BaseController
     end
   end
 
+  # PATCH/PUT /api/v1/cards/:id
+  def update
+    card = current_user.cards.find(params[:id])
+
+    if card.update(card_params)
+      render json: card.as_json(only: %i[id content logged_date]), status: :ok
+    else
+      render json: { errors: card.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
     # 属性を限定してハッシュ化
@@ -41,6 +52,7 @@ class Api::V1::CardsController < Api::V1::BaseController
       resource.as_json(only: %i[id content logged_date])
     end
 
+    # card 以下のパラメータから content と logged_date のみを許可して安全に受け取る
     def card_params
       params.require(:card).permit(:content, :logged_date)
     end
