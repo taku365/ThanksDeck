2eebe428117df7fc77e3fac8ad56e4f0e903f8b8
taku365 <stcd1097@gmail.com>
2025-06-22 22:18:23 +0900

    Create / Update / Destroy のリクエストスペック実装

--- rails/spec/support/auth_helpers.rb
+++ rails/spec/support/auth_helpers.rb
@@ -1,7 +1,6 @@
 module AuthHelpers
-  # メソッド定義
   def auth_headers_for(user)
-    post "/api/v1/auth/sign_in", # PostはRequest Spec が提供しているメソッドを呼び出している
+    post "/api/v1/auth/sign_in",
          params: { email: user.email, password: user.password }.to_json,
          headers: { "Content-Type" => "application/json" }
 
