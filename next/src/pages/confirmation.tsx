3e340ac208b61cbfd4e7049484cb0cbfd348431b
taku365 <stcd1097@gmail.com>
2025-06-25 18:13:15 +0900

    認証リンク修正

---
+++ next/src/pages/confirmation.tsx
@@ -0,0 +1,61 @@
+import { Alert, Container, Typography } from '@mui/material'
+import { AxiosError, isAxiosError } from 'axios'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { api } from '@/utils/api'
+
+export default function ConfirmationPage() {
+  const router = useRouter()
+  const { confirmation_token } = router.query
+  const [isLoading, setIsLoading] = useState(true)
+  const [successMessage, setSuccessMessage] = useState<string>('')
+  const [errorMessage, setErrorMessage] = useState<string>('')
+
+  useEffect(() => {
+    if (!router.isReady || !confirmation_token) {
+      return;
+    }
+
+    const confirmAccount = async () => {
+      try {
+        await api.patch("/user/confirmations", null, {
+          params: { confirmation_token },
+        });
+        setSuccessMessage(
+          "アカウントが有効化されました。サインインページへ移動します。"
+        );
+        // 1秒後にサインインへリダイレクト
+        setTimeout(() => router.push("/signin"), 1000);
+      } catch (e) {
+        if (isAxiosError(e)) {
+          const axiosError = e as AxiosError<{ message: string }>;
+          setErrorMessage(
+            axiosError.response?.data?.message ??
+              "アカウントの有効化に失敗しました。"
+          );
+        } else {
+          setErrorMessage("予期せぬエラーが発生しました");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    confirmAccount();
+  }, [router, router.isReady, confirmation_token]);
+
+  return (
+    <Container maxWidth="xs" sx={{ mt: 8 }}>
+      {isLoading && <Typography>有効化中…</Typography>}
+      {!isLoading && successMessage && (
+        <Alert severity="success" sx={{ mb: 2 }}>
+          {successMessage}
+        </Alert>
+      )}
+      {!isLoading && errorMessage && (
+        <Alert severity="error" sx={{ mb: 2 }}>
+          {errorMessage}
+        </Alert>
+      )}
+    </Container>
+  )
+}
