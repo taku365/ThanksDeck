3a657c3dd4c1188e27987dcd18068513768d9eb8
taku365 <stcd1097@gmail.com>
2025-06-27 13:36:40 +0900

    月別カード一覧の取得

--- next/src/pages/cards.tsx
+++ next/src/pages/cards.tsx
@@ -1,28 +1,65 @@
 import dayjs from 'dayjs'
 import dynamic from 'next/dynamic'
-import { useState } from 'react'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
+import useSWR from 'swr'
+import Layout from './components/Layout'
+import { fetcher } from '@/utils/api'
 
+// Hydration エラー回避
 const CalendarMini = dynamic(() => import('./components/CalendarMini'), {
   ssr: false,
 })
 
-export default function MonthlyEntriesPage() {
-  const [year, setYear] = useState(dayjs().year())
-  const [month, setMonth] = useState(dayjs().month() + 1)
+export interface Card {
+  id: number
+  content: string
+  logged_date: string
+}
 
-  const markedDates = ['2025-06-19', '2025-06-18']
+export default function MonthlyDeckPage() {
+  const router = useRouter()
+  const today = dayjs()
+  const [currentYear, setCurrentYear] = useState(
+    Number(router.query.year ?? today.year()),
+  )
+  const [currentMonth, setCurrentMonth] = useState(
+    Number(router.query.month ?? today.month() + 1),
+  )
+
+  const { data: MonthlyCards = [], error } = useSWR(
+    `/cards/deck/${currentYear}/${currentMonth}`,
+    fetcher,
+  )
+
+  // ['2025-06-19', '2025-06-18']
+  const markedDates = MonthlyCards.map((c: Card) => c.logged_date)
+
+  if (!MonthlyCards && !error) {
+    return (
+      <p>
+        Loading cards for {currentYear}/{currentMonth}...
+      </p>
+    )
+  }
+  if (error) {
+    return <p>Error loading cards.</p>
+  }
+  console.log(MonthlyCards)
 
   return (
-    <CalendarMini
-      year={year}
-      month={month}
-      markedDates={markedDates}
-      onChangeMonth={(y, m) => {
-        setYear(y)
-        setMonth(m)
-        // ここでAPI再フェッチ
-      }}
-      onSelectDate={(date) => console.log('選択日:', date)}
-    />
+    <Layout>
+      <CalendarMini
+        year={currentYear}
+        month={currentMonth}
+        markedDates={markedDates}
+        onChangeMonth={(y, m) => {
+          setCurrentYear(y)
+          setCurrentMonth(m)
+          // ここでAPI再フェッチ
+        }}
+        onSelectDate={(date) => console.log('選択日:', date)}
+      />
+    </Layout>
   )
 }
