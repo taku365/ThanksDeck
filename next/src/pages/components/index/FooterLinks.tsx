e588dcb0871cf3701a8980eb41209260c5a76cdb
taku365 <stcd1097@gmail.com>
2025-06-28 17:19:26 +0900

    ヒーローセクション作成

---
+++ next/src/pages/components/index/FooterLinks.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function FooterLinks() {
+  return <div>FooterLinks</div>
+}
--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,15 +1,25 @@
 import { Button } from '@mui/material'
 import { useRouter } from 'next/router'
 import React from 'react'
+import Layout from './components/Layout'
+import FeatureSection from './components/index/FeatureSection'
+import FooterLinks from './components/index/FooterLinks'
+import HeroSection from './components/index/HeroSection'
 
 export default function PublicThanksDeck() {
   const router = useRouter()
 
   return (
     <>
-      <div>ここで新規登録やログイン</div>
-      <Button onClick={() => router.push('/signup')}>新規登録ボタン</Button>
-      <Button onClick={() => router.push('/signin')}>ログイン</Button>
+      <Layout>
+        <HeroSection />
+        <FeatureSection />
+        <FooterLinks />
+
+        <div>ここで新規登録やログイン</div>
+        <Button onClick={() => router.push('/signup')}>新規登録ボタン</Button>
+        <Button onClick={() => router.push('/signin')}>ログイン</Button>
+      </Layout>
     </>
   )
 }
