c11c1a9094b0a7de4aef3f47e7b58b3b66a9e8ce
taku365 <stcd1097@gmail.com>
2025-07-08 22:03:32 +0900

    テスト時はredisに接続しない設定

--- rails/config/environments/test.rb
+++ rails/config/environments/test.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # テスト時はredisに接続しない
+  config.active_job.queue_adapter = :inline
 end
