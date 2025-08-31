0440491ba9dfab272a0e617320bdaef222470585
taku365 <stcd1097@gmail.com>
2025-08-31 11:10:45 +0900

    編集時にチャッピーの返信が更新されるよう修正

--- rails/app/jobs/generate_reply_job.rb
+++ rails/app/jobs/generate_reply_job.rb
@@ -1,14 +1,12 @@
 class GenerateReplyJob < ApplicationJob
-  queue_as :default # Sidekiq の default キューに入る
+  queue_as :default
 
   def perform(card_id)
     card = Card.find(card_id)
-    return if card.reply.present? # 既に生成済みなら何もしない
-
     reply_text = OpenaiClient.generate_reply(card.content)
     card.update!(reply: reply_text)
   rescue => e
     Rails.logger.error "GenerateReplyJob failed: #{e.message}"
-    raise e # Sidekiq の自動リトライを使う
+    raise e
   end
 end
--- rails/app/models/card.rb
+++ rails/app/models/card.rb
@@ -4,12 +4,19 @@ class Card < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
   validates :logged_date, presence: true
 
-  # カード保存後にジョブをキック
-  after_commit :enqueue_reply_job, on: :create
+  # カード作成・編集後にジョブをキック
+  after_create_commit :enqueue_reply_job
+  after_update_commit :enqueue_reply_job_if_content_changed
 
   private
 
     def enqueue_reply_job
+      GenerateReplyJob.perform_later(id)
+    end
+
+    def enqueue_reply_job_if_content_changed
+      return unless previous_changes.has_key?("content")
+
       GenerateReplyJob.perform_later(id)
     end
 end
