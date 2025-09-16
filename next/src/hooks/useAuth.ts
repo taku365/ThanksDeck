a0b7539221ef4377df96526b585e0099bd333242
taku365 <stcd1097@gmail.com>
2025-09-16 12:37:50 +0900

    Layout変更

--- next/src/hooks/useAuth.ts
+++ next/src/hooks/useAuth.ts
@@ -1,3 +1,4 @@
+import { isAxiosError } from 'axios'
 import { useCallback } from 'react'
 import { api } from '../utils/api'
 import { clearAuthTokens } from '../utils/tokenStorage'
@@ -35,10 +36,27 @@ export const useAuth = () => {
     [],
   )
 
+  // const signOut = useCallback(async () => {
+  //   await api.delete('/auth/sign_out')
+  //   clearAuthTokens()
+  //   await refresh(undefined, { revalidate: false })
+  // }, [refresh])
   const signOut = useCallback(async () => {
-    await api.delete('/auth/sign_out')
-    clearAuthTokens()
-    await refresh(undefined, { revalidate: false })
+    try {
+      await api.delete('/auth/sign_out')
+    } catch (e) {
+      if (
+        !(
+          isAxiosError(e) &&
+          (e.response?.status === 401 || e.response?.status === 404)
+        )
+      ) {
+        throw e
+      }
+    } finally {
+      clearAuthTokens()
+      await refresh(undefined, { revalidate: false })
+    }
   }, [refresh])
 
   const guestSignIn = useCallback(async () => {
